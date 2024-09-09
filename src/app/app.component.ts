fce5508d70a6b483379ea14bcceb64bbf47e38d2
Felipe Elvas Barbosa <felipeelvas@gmail.com>
2024-09-08 23:01:51 -0300

    first commit~

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
+import { IUser } from './interfaces/user.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'async-pipe-promise-184';
+export class AppComponent implements OnInit {
+  userPromise: Promise<IUser> | undefined;
+  userByIdPromise: Promise<IUser> | undefined;
+  constructor(
+    private readonly _usersService: UsersService
+  ){}
+
+  ngOnInit() {
+    this.userPromise = this._usersService.getUser();
+
+    this.userByIdPromise = lastValueFrom(this._usersService.getUserById(2));
+
+    this._usersService.getUser().then(user => {
+      console.log('User ->', user);
+    })
+  }
 }
